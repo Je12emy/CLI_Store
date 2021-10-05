30b5f9f23add3fa579d0897c7ba979a715b9d330
Je12emy <Jeremyzelaya@hotmail.es>
2021-10-05 16:40:43 -0600

    Add Voyager documentation decorators

--- server/Models/Platform.cs
+++ server/Models/Platform.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using HotChocolate;
 
 namespace server.Models
 {
+    [GraphQLDescription("Represent a software or service CLI")]
     public class Platform
     {
         [Key]
@@ -11,6 +13,7 @@ namespace server.Models
         [Required]
         public string Name { get; set; }
 
+        [GraphQLDescription("Represent a purchased valid licence")]
         public string LicenseKey { get; set; }
 
         public ICollection<Command> Commands { get; set; } = new List<Command>();
